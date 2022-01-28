ed0a142c68bad0d9206de4587e4fe179b3fc3f09
Emma Williams <github@em01.co.uk>
2022-01-28 15:30:13 +0000

    speech recognizer

--- app/src/main/java/com/emma/testtts/MainActivity.kt
+++ app/src/main/java/com/emma/testtts/MainActivity.kt
@@ -1,109 +1,120 @@
 package com.emma.testtts
 
-import android.app.Activity
-import android.content.ActivityNotFoundException
-import android.content.ContentValues.TAG
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.SpeechRecognizer
 
 import android.speech.RecognizerIntent
-import android.speech.tts.TextToSpeech
-import android.util.Log
 import android.view.MotionEvent
-import android.widget.Button
+import android.view.View
 import android.widget.EditText
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 
-class GoogleAssistantMainActivityExample : AppCompatActivity() {
-    lateinit var btn_stt : Button
-    lateinit var et_text_input : EditText
-    private var speechRecognizer: SpeechRecognizer? = null
 
+class MainActivity : AppCompatActivity() {
+    lateinit var speechRecognizer: SpeechRecognizer
+
+    lateinit var micButton: ImageView
+    lateinit var editText: EditText
 
     //The intent holds the recognized text
 
     companion object {
         private const val REQUEST_CODE_STT = 1
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
-        speechRecognizer?.setRecognitionListener(listener)
-
         setContentView(R.layout.activity_main)
-        btn_stt = findViewById(R.id.btn_stt)
 
-        btn_stt.setOnClickListener {
-            val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-            //use the langauge model to define the purpose.
-            sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission()
+        }
 
-            //Any langauge can be used from the locale class.
-            sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-            sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something now!")
-            speechRecognizer?.startListening(sttIntent)
+        editText = findViewById(R.id.text);
+        micButton = findViewById(R.id.button);
 
-            try {
-                getResult.launch(sttIntent)
-//                startActivityForResult(sttIntent, REQUEST_CODE_STT)
-            } catch (e: ActivityNotFoundException) {
-                e.printStackTrace()
-                Toast.makeText(this, "Device does not support stt.", Toast.LENGTH_LONG).show()
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        val sttIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something now!")
+
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onBeginningOfSpeech() {
+                editText.setText("")
+                editText.setHint("Listening")
             }
-        }
-    }
 
-    private var listener:RecognitionListener = object: RecognitionListener{
+            override fun onPartialResults(partialResults: Bundle?) {
 
-
-        override fun onBeginningOfSpeech() {
-        }
-
-        override fun onPartialResults(partialResults: Bundle?) {
-
-        }
-
-        override fun onResults(results: Bundle?) {
-
-        }
-
-        override fun onEvent(eventType: Int, params: Bundle?) {
-
-        }
-
-        override fun onEndOfSpeech() {
-        }
-
-        override fun onError(error: Int) {
-        }
-
-        override fun onBufferReceived(buffer: ByteArray?) {
-        }
-
-        override fun onReadyForSpeech(params: Bundle?) {
-        }
-
-        override fun onRmsChanged(rmsdB: Float) {
-        }
-    }
-
-    private val getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == Activity.RESULT_OK && it.data !== null) {
-            val result = it.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
-
-            if(!result.isNullOrEmpty()) {
-                val recognizedText = result[0]
-                et_text_input = findViewById(R.id.et_text_input)
-                et_text_input.setText(recognizedText)
             }
-        }
+
+            override fun onResults(results: Bundle?) {
+                micButton.setImageResource(R.drawable.ic_mic_off)
+                val data = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) ?: arrayListOf()
+                editText.setText(data[0])
+            }
+
+            override fun onEvent(eventType: Int, params: Bundle?) {
+
+            }
+
+            override fun onEndOfSpeech() {
+            }
+
+            override fun onError(error: Int) {
+            }
+
+            override fun onBufferReceived(buffer: ByteArray?) {
+            }
+
+            override fun onReadyForSpeech(params: Bundle?) {
+            }
+
+            override fun onRmsChanged(rmsdB: Float) {
+            }
+        })
+
+        micButton.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+                when (event?.action) {
+                    MotionEvent.ACTION_UP -> {
+                        speechRecognizer.stopListening()
+                        editText.hint = "You will see the speech here"
+                    }
+                    MotionEvent.ACTION_DOWN -> {
+                        micButton.setImageResource(R.drawable.ic_mic)
+                        editText.hint = "Listening..."
+                        speechRecognizer.startListening(sttIntent)
+                    }
+                }
+                return false
+            }
+        })
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        speechRecognizer!!.destroy()
+    }
 
+    private fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.RECORD_AUDIO),
+                REQUEST_CODE_STT
+            )
+        }
+    }
 }
